6b4c68a55b58a025884f14dead3456adb8bd473d
Parthkarir <parthcallsmaster@gmail.com>
2026-06-20 18:52:38 +0530

    socketcontext

--- Backend/controllers/createConversation.Controller.js
+++ Backend/controllers/createConversation.Controller.js
@@ -14,8 +14,6 @@ export const createConversation = async (req, res) => {
       });
     }
 
-
-
     const existinguser = await createUser.findOne({ where: { email: email } });
 
     if (!existinguser) {
@@ -33,67 +31,73 @@ export const createConversation = async (req, res) => {
         success: false,
       });
     }
-    console.log("yaha tk chlaaaa")
+    console.log("yaha tk chlaaaa");
     const existingconversation = await conversation_members.findAll({
       where: {
         user_id: { [Op.in]: [loggedInUserId, existinguser.id] },
       },
     });
 
+    const conversationIds = existingconversation.map((item) => {
+      return item.conversation_id;
+    });
 
-    const conversationIds = existingconversation.map((item)=>{
-      return item.conversation_id
-    })
-
-    const countIds = conversationIds.reduce((acc,curr)=>{
-      if(acc[curr]){
+    const countIds = conversationIds.reduce((acc, curr) => {
+      if (acc[curr]) {
         acc[curr] = acc[curr] + 1;
-      }else {
+      } else {
         acc[curr] = 1;
       }
 
       return acc;
+    }, {});
 
-    },{})
+    const sameIds = Object.entries(countIds)
+      .filter(([conversationId, count]) => {
+        return count == 2;
+      })
+      .map(([conversationId]) => {
+        return Number(conversationId);
+      });
 
-    const sameIds = Object.entries(countIds).filter(([conversationId,count])=>{
-      return count==2
-    }).map(([conversationId])=>{
-      return Number(conversationId)
-    })
-    
+    const isConversationAlreadyExist = await conversation.findAll({
+      where: {
+        id: { [Op.in]: sameIds },
+        isGroup: false,
+      },
+    });
 
+    console.log(isConversationAlreadyExist, "kya ayaaaa");
 
-
-    return res.status(200).json({
-      message : "jatin ji",
-      data : sameIds
-    })
-
-
-
-    if (existingconversation.length>0) {
+    if (isConversationAlreadyExist.length > 0) {
       return res.status(400).json({
         message: "Conversation already exists",
         success: false,
       });
     }
 
-    const ConversationId =  await conversation.create({});
+    const Conversation = await conversation.create({});
 
-    const date = new Date();
+    const Bothids = {
+      user_id: loggedInUserId,
+      conversation_id: Conversation.id,
+      joined_at: new Date(),
+    };
 
+    const Converation_members = await conversation_members.bulkCreate(
+      [{
+        user_id: loggedInUserId,
+        conversation_id: Conversation.id,
+        joined_at: new Date(),
+      },
+      {
+        user_id: existinguser.id,
+        conversation_id: Conversation.id,
+        joined_at: new Date(),
+      }]
 
+    );
 
-    const Bothids = { user_id :  loggedInUserId,conversation_id : ConversationId.id ,joined_at : date}
-
-   const Converation_members =  await conversation_members.create(Bothids)
-
-
-
-    console.log(existingconversation, "iidhihi");
-
-    
 
     return res.status(200).json({
       message: "Conversation created successfully",
