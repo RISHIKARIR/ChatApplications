d758c3cb02203341fd93ecf04f306163d1f705c9
Parthkarir <parthcallsmaster@gmail.com>
2026-06-26 18:03:32 +0530

    creating group create api

--- Backend/routes/conversations.route.js
+++ Backend/routes/conversations.route.js
@@ -1,15 +1,14 @@
 import express from "express";
 import { showConversations, } from "../controllers/showConversationController.js";
 import { verifytoken } from "../middlewares/auth.middlewares.js";
-import { createConversation } from "../controllers/createConversation.Controller.js";
+import { createConversation,createGroup } from "../controllers/createConversation.Controller.js";
 
 const router = express.Router();
 
 
 router.get("/conversations",verifytoken,showConversations)
 router.post("/conversations",verifytoken,createConversation)
-
-
+router.post("/conversations/group",verifytoken,createGroup)
 
 
 
