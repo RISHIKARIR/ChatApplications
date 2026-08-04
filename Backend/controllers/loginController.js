caadff36e91185a9c532ff31f6a4591d91ac4b7c
Parth <rishikarir0@gmail.com>
2026-08-04 22:03:24 +0530

    Set SameSite attribute for cookies in loginController

--- Backend/controllers/loginController.js
+++ Backend/controllers/loginController.js
@@ -61,12 +61,14 @@ export const login = async (req, res) => {
     res.cookie("accesstoken", accesstoken, {
       httpOnly: true,
       secure: true,
+      sameSite : "none",
       maxAge:  15 * 60 * 1000,
     });
 
     res.cookie("refreshtoken", refreshtoken, {
       httpOnly: true,
       secure: true,
+      sameSite : "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
