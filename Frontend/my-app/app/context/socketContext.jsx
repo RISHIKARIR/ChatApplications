e0604db25d320a14c8b904c1ea1d6787613ddeb7
Parth <rishikarir0@gmail.com>
2026-08-04 23:54:49 +0530

    Update socketContext.jsx

--- Frontend/my-app/app/context/socketContext.jsx
+++ Frontend/my-app/app/context/socketContext.jsx
@@ -35,7 +35,7 @@ export function SocketProvider({ children }) {
 
     if (socketRef?.current?.connected) return;
 
-    const socket = io(process.env.BACKEND_URL, {
+    const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
       withCredentials: true,
       query: {
         UserId: user?.id,
