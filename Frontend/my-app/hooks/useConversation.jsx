e39ebe5d7ab8fbe245599bff05d4150411fbb606
Parth <rishikarir0@gmail.com>
2026-08-20 22:13:06 +0530

    working has been done

--- Frontend/my-app/hooks/useConversation.jsx
+++ Frontend/my-app/hooks/useConversation.jsx
@@ -1,45 +1,88 @@
 import { useEffect } from "react";
 
+export const useSocketConversation = (
+  socketRef,
+  setConversationUserData,
+  setFilteredData,
+) => {
+  const updateLastMessage = (data) => {
+    setConversationUserData((prev) => {
+      return prev?.map((item) => {
+        if (item.id == data.conversationId) {
+          return {
+            ...item,
+            lastmessage: data.lastmessage,
+            lastmessageDate: data.lastmessageDate,
+          };
+        }
+        return item;
+      });
+    });
 
-export const useSocketConversation = (socketRef,setConversationUserData,setFilteredData) => {
-    
- 
-        const updateLastMessage = (data)=>{
+    setFilteredData((prev) => {
+      return prev?.map((item) => {
+        if (item.id == data.conversationId) {
+          return {
+            ...item,
+            lastmessage: data.lastmessage,
+            lastmessageDate: data.lastmessageDate,
+          };
+        }
+        return item;
+      });
+    });
+  };
 
+  const removeUnreadCount = (data) => {
+    const { conversationId } = data;
 
+    setConversationUserData((prev) => {
+      return prev.map(({ user_members, ...rest }) => {
+        return {
+          ...rest,
+          user_members: user_members.map((item) => {
+            if (item.conversation_members.conversation_id == conversationId) {
+              return { ...item, unread_count: 0 };
+            }
 
-            setConversationUserData((prev)=>{
-              return prev?.map((item)=>{
-                    if(item.id == data.conversationId){
-                        return {...item,lastmessage :data.lastmessage,lastmessageDate : data.lastmessageDate}
-                    }
-                    return item;
-
-                })
-            })
-
-                setFilteredData ((prev)=>{
-              return prev?.map((item)=>{
-                    if(item.id == data.conversationId){
-                        return {...item,lastmessage :data.lastmessage,lastmessageDate : data.lastmessageDate}
-                    }
-                    return item;
-
-                })
-            })
-
-
-
-
-
-    }
-        
-        
-    
-        socketRef?.current?.on("update_Conversation",updateLastMessage)
+            return item;
+          }),
+        };
+      });
+    });
 
 
 
 
+    setFilteredData((prev)=>{
+        return prev.map(({user_members,...rest })=>{
+            return {...rest,
+            user_members :  user_members.map((item)=>{
+                if(item.conversation_members.conversation_id == conversationId){
+                return {...item,unread_count : 0}  
+            }  
+
+            return item;
+})
 
 }
+
+        })
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+  };
+
+  socketRef?.current?.on("update_Conversation", updateLastMessage);
+  socketRef?.current?.on("remove_unread_count", removeUnreadCount);
+};
